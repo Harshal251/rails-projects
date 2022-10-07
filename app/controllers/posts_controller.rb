79f527d2123766ae68555cd5bf4acba41b0dc4a7
Harshal <harshal@webkorps.com>
2022-10-08 03:02:36 +0530

    functionalities added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,50 @@
 class PostsController < ApplicationController
-    def index
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = 'Post was successfully created'
+      redirect_to @post
+    else
+      render action: 'new'
     end
+  end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 
-    def new 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render action: 'edit'
     end
+  end
 
-    def create
-    #   @post = Post.new(post_params)
-    end
+  def edit
+    @post = Post.find(params[:id])
+  end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
